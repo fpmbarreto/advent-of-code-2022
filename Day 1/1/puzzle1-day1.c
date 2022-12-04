88852098418d8bee355582efa95564e48032dd50
Francisco Barreto <fbarreto@ubiwhere.com>
2022-12-04 18:10:21 +0000

    Read and print .txt file

--- Day 1/1/puzzle1-day1.c
+++ Day 1/1/puzzle1-day1.c
@@ -2,5 +2,19 @@
 #include "stdio.h"
 
 void main(void){
+    FILE* fp;
+    fp = fopen("input.txt", "r");
     
+    char str[7];
+
+    if(fp == NULL){
+        printf("Error opening .txt file");
+    }
+
+    printf("List of calories:\n\r");
+
+    while(fgets(str, sizeof(str),fp)){
+        puts(str);
+        printf("%s",str);
+    }
 }
